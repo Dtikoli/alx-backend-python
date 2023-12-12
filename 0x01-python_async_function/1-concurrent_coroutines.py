b049cce24c9d8997d0e94860157615524f51b646
Dtikoli <tikolidavid@yahoo.com>
2023-12-12 06:08:04 -0800

    Adding modification

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -16,5 +16,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         list of delays
     """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    tasks = [wait_random(max_delay) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
